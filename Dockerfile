e9a87249b4f08c218d68684eaf16251b33072802
adam malantonio <malantoa@lafayette.edu>
2020-07-22 17:42:15 -0400

    add mountable storage (#2)
    * allow more flexible storage configuration via ENV
    * cleanup installation script
    * first pass looks ok

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM php:7.2-apache
 LABEL maintainer="malantoa@lafayette.edu"
 
 ARG VERSION="latest"
+ARG CUSTOM_SETTINGS_FILE="./config/local_settings_custom.php"
 
 # enable rewrites early
 RUN a2enmod rewrite
@@ -26,8 +27,16 @@ RUN docker-php-ext-install -j$(nproc) iconv pdo pdo_mysql mysqli gd intl zip exi
 RUN pecl install mcrypt-1.0.2 && docker-php-ext-enable mcrypt && pecl install imagick && docker-php-ext-enable imagick
 
 COPY --chown=1 scripts/ /scripts
-
 RUN /scripts/install-scalar.sh
+
+# need to copy custom config _after_ installing scalar
+COPY --chown=${APACHE_RUN_USER:-www-data}:${APACHE_RUN_GROUP:-www-data} ${CUSTOM_SETTINGS_FILE} /var/www/html/system/application/config/local_settings_custom.php
+
+# @todo can we do this without symlinking?
+RUN mkdir -p /scalar-storage \
+    && chown -R ${APACHE_RUN_USER:-www-data}:${APACHE_RUN_GROUP:-www-data} /scalar-storage \
+    && ln -s /scalar-storage /var/www/html/uploads
+VOLUME /scalar-storage
 
 ENTRYPOINT ["/scripts/docker-entrypoint.sh"]
 CMD ["apache2-foreground"]
--- scripts/install-scalar.sh
+++ scripts/install-scalar.sh
@@ -1,35 +1,24 @@
 #!/bin/bash
 set -euo pipefail
 
-install_directory="/var/www/html"
-
-fetch_latest_tag () {
-  curl -s "https://api.github.com/repos/anvc/scalar/releases/latest" \
-    | grep '"tag_name":' \
-    | sed -E 's/.*"([^"]+)".*/\1/'
-}
-
-install_scalar () {
-  local version="$1"
-  local install_directory="/var/www/html"
-  local archive_url_template="https://github.com/anvc/scalar/archive/%s.tar.gz"
-
-  if ! curl -Lfs --output /dev/null $(printf "$archive_url_template" "$version"); then
-    echo "Requested Scalar version $version doesn't exist"
-    exit 1
-  fi
-
-  curl -sL -o /tmp/scalar.tar.gz $(printf "$archive_url_template" "$version")
-  rm -rf "$install_directory/*"
-
-  tar -xvzf /tmp/scalar.tar.gz -C "$install_directory" --strip-components=1
-  chown -R ${APACHE_RUN_USER:-www-data}:${APACHE_RUN_GROUP:-www-data} "$install_directory"
-
-  echo "Installed Scalar $version to $install_directory"
-}
-
+# default to the most recent tagged version if no version provided
 if [[ -z ${VERSION+x} || "$VERSION" = "latest" ]]; then
-  VERSION=$(fetch_latest_tag)
+  VERSION=$(curl -s "https://api.github.com/repos/anvc/scalar/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 fi
 
-install_scalar "$VERSION"
+install_directory="/var/www/html"
+archive_url="https://github.com/anvc/scalar/archive/$VERSION.tar.gz"
+
+if ! curl -Lfs --output /dev/null "$archive_url";
+then
+  echo "Requested Scalar version $VERSION doesn't exist"
+  exit 1
+fi
+
+rm -rf "$install_directory/*"
+curl -sL "$archive_url" | tar -xz -C "$install_directory" --strip-components=1
+
+chown -R "${APACHE_RUN_USER:-www-data}:${APACHE_RUN_GROUP:-www-data}" "$install_directory"
+sed -i'' -e 's/^#\(.*\%{DOCUMENT_ROOT}\/uploads\/.*\)/\1/g' "$install_directory/.htaccess"
+
+echo "Installed Scalar $VERSION to $install_directory"
