fa33b53c54602707669ab437fadbfab9a9137d54
adam malantonio <malantoa@lafayette.edu>
2020-07-23 10:01:52 -0400

    uncomment .htaccess lines per scalar's rec.

--- scripts/install-scalar.sh
+++ scripts/install-scalar.sh
@@ -19,6 +19,18 @@ rm -rf "$install_directory/*"
 curl -sL "$archive_url" | tar -xz -C "$install_directory" --strip-components=1
 
 chown -R "${APACHE_RUN_USER:-www-data}:${APACHE_RUN_GROUP:-www-data}" "$install_directory"
-sed -i'' -e 's/^#\(.*\%{DOCUMENT_ROOT}\/uploads\/.*\)/\1/g' "$install_directory/.htaccess"
+
+# uncomment rows of the scalar .htaccess file:
+# 1. serve files in %{DOCUMENT_ROOT}/uploads if they exist
+# 2-5: "To protect against malicious file uploads (e.g., PHP files) uncomment these lines
+#       and make sure to set '/scalar' in the second line to the path to your Scalar install"
+sed -i'' \
+    -e 's/^#\(.*\%{DOCUMENT_ROOT}\/uploads\/.*\)/\1/g' \
+    -e 's/^#\(RewriteCond \%{REQUEST_FILENAME} -f\)/\1/' \
+    -e 's/^#\(RewriteCond \%{REQUEST_URI}.*\)\/scalar\/system\(.*\)/\1\/var\/www\/html\/system\2/' \
+    -e 's/^#\(RewriteCond \%{REQUEST_FILENAME} !codeigniter\.php \[NC\]\)/\1/' \
+    -e 's/^#\(RewriteCond \%{REQUEST_FILENAME} (\\\.php)\$ \[NC\]\)/\1/' \
+    -e 's/^#\(RewriteRule \^(\.\*)\$ - \[R=404,L\]\)/\1/' \
+    "$install_directory/.htaccess"
 
 echo "Installed Scalar $VERSION to $install_directory"
